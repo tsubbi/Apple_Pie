485404b7b97ae566fb332601be1021ba2cd79c8f
Jamie <jamie.yjchen@gmail.com>
2021-04-26 21:59:08 -0700

    add Game struct

---
+++ ApplePie/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Jamie Chen on 2021-04-26.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
