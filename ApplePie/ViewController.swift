fae21ac2f8275ea52b8ae6e582ccc1b0f4b1aee6
Jamie <jamie.yjchen@gmail.com>
2021-04-26 21:05:23 -0700

    finish up to part one of the project

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 }
 
